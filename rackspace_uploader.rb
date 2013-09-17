fd6e6d27e775dd491c2768c3ca36ce48190e1d13
Nathan Wenneker <nathan@gladtocode.com>
2013-09-16 19:11:23 -0600

    another fix

--- rackspace_uploader.rb
+++ rackspace_uploader.rb
@@ -11,7 +11,7 @@ module LJV
     def upload(filepath, container_name, object_name)
       container = connection.container(container_name)
       object = container.create_object(object_name)
-      object.load_from_filename(backup_filepath)
+      object.load_from_filename(filepath)
       puts "Uploaded #{filepath} to rackspace"
     end
 
